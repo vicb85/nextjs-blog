e262064c558e52fcdf00dd9726f9d5279b82f026
victor.belles <victor.belles@promocionesfarma.com>
2023-10-06 13:04:42 +0200

    training

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import MarkdownPreview from "@/components/markdownPreview";
+import getPost from "@/services/getPost";
+
+const PostPage = ({ params: { slug } }: { params: { slug: string } }) => {
+    const post = getPost(slug);
+
+    return (
+      <div className="flex flex-col space-y-2">
+        <h1 className="text-xl font-semibold">{post.title}</h1>
+        <p>{post.category} - {post.author} - {post.date}</p>
+        <MarkdownPreview source={post.content} />
+      </div>
+    );
+  };
+
+  export default PostPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-const HomePage = () => {};
+import getPosts from "@/services/getPosts";
+import Link from "../../node_modules/next/link";
+
+const HomePage = () => {
+    const posts = getPosts();
+
+      return (
+        <div className="flex flex-col space-y-2">
+          {posts.map(post => (
+            <div>
+              <Link href={post.slug}>
+                <h2>{post.title}</h2>
+              </Link><p>{post.description}</p>
+            </div>
+
+          ))}
+        </div>
+      );
+};
 
 export default HomePage;
